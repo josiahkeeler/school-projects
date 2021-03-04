2e658e7c43368916d27d1fd1adcf2805e9bf900d
josiahkeeler <josiahkeeler@pop-os.localdomain>
2021-03-04 15:57:44 -0800

    working on printing hex and ptrs

--- printf/ft_convert_numbers.c
+++ printf/ft_convert_numbers.c
@@ -1,29 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_flags.c                                         :+:      :+:    :+:   */
+/*   ft_convert_numbers.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: josiahkeeler <josiahkeeler@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/26 16:10:51 by josiahkeele       #+#    #+#             */
-/*   Updated: 2021/02/27 18:21:11 by josiahkeele      ###   ########.fr       */
+/*   Created: 2021/03/04 15:31:21 by josiahkeele       #+#    #+#             */
+/*   Updated: 2021/03/04 15:57:21 by josiahkeele      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-/* flags are "-0.*"
-- left justifies the field
-0 Left-pads the number with zeroes (0) instead of spaces
-* The width is not specified in the format string, 
-	but as an additional integer value argument preceding the argument 
-	that has to be formatted.
-. precision field */
-const char *ft_flags(const char *input)
+void	ft_put_hex(unsigned int n) //takes unsigned int
 {
-	while (ft_strchr(FLAGS, *input))
+
+}
+void	ft_convert_hex(const *input, va_list ap)
+{
+	if (*input && (*input == 'x' || *input == 'X'))
 	{
-		input++;
+
+	} 
+}
+void	ft_convert_ptr(const *input, va_list ap)
+{
+	void *ptr;
+
+	if (*input && *input == 'p')
+	{
+		ptr = (void *)va_arg(ap, void *); //ptr is unsigned long (8 bytes)
+		
 	}
-	return (input);
 }
