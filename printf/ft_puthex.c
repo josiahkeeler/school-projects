a87ff9e048d59e29d1422de15b5afd9054cd98a8
josiahkeeler <josiahkeeler@pop-os.localdomain>
2021-03-13 22:10:08 -0800

    finished ft_puthex

--- printf/ft_puthex.c
+++ printf/ft_puthex.c
@@ -3,40 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   ft_puthex.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: josiahkeeler <josiahkeeler@student.42.f    +#+  +:+       +#+        */
+/*   By: jokeeler <jokeeler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/05 16:58:50 by josiahkeele       #+#    #+#             */
-/*   Updated: 2021/03/11 17:08:32 by josiahkeele      ###   ########.fr       */
+/*   Created: 2021/03/13 22:07:18 by jokeeler          #+#    #+#             */
+/*   Updated: 2021/03/13 22:08:59 by jokeeler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h>
-
 
 void	ft_put_hex(unsigned int n, char specifier) //takes unsigned int
 {
 	char hex[9];
 	int i;
+	unsigned int loop;
+	char *base16;
 
 	i = 0;
+	loop = 1;
 	ft_bzero(hex, 9);
-	if (specifier == 'X')
+	base16 = specifier == 'X' ? HEX_UP : HEX_LOW;
+	while (loop)
 	{
-		while (n > 0) // doesn't work when n == 0
-		{
-			hex[i++] = HEX_UP[n % 16];
-			n /= 16;
-		}
+		hex[i++] = base16[n % 16];
+		n /= 16;
+		loop = n;
 	}
-	while (i-- > 0)
-		ft_putchar_fd(hex[i], 1);
-}
-
-int	main(void)
-{
-	int x = 0;
-	ft_put_hex(x, 'X');
-	printf("\n%X\n", x);
-	return (0);
+	while (i)
+		ft_putchar_fd(hex[--i], 1);
 }
